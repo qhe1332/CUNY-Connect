0072dcbc6c42ce848f92493508f4deeea63d2b84
Cherry <cherryhe5@gmail.com>
2021-08-06 00:07:51 -0400

    create server

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
+const host = 'localhost';
+const port = 8080;
+
+http.createServer(function(req, res)
+{
+	res.writeHead(200, {'Content-Type': 'text/plain'})
+	fs.readFile('index.html', function(err, data)
+	{
+		if (err)
+		{
+			console.log(err)
+			res.writeHead(404)
+			res.write('Error: File Not Found')
+		}
+		else
+		{
+			res.writeHead(200, {"Content-Type": "text/html"})
+			res.write(data)
+		}
+	})
+    res.end()
+}).listen(port)
+
+console.log(`Server is running on http://${host}:${port}`)
